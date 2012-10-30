1ce6bbdb1ddca016e64f0143d3c86e82df8f1a4d
Marius Hein <marius.hein@netways.de>
2012-10-30 16:34:46 +0100

    Add initialdata, added scrip implementation

--- lib/RT/Action/ChangeOwner.pm
+++ lib/RT/Action/ChangeOwner.pm
@@ -1,4 +1,4 @@
-package RT::Action::SetOwner;
+package RT::Action::ChangeOwner;
 
 use strict;
 use warnings;
@@ -12,6 +12,12 @@ sub Prepare {
     my $self = shift;
     RT->Logger->debug(__PACKAGE__. ": Prepare");
     RT->Logger->debug(__PACKAGE__. "::Argument: ". $self->Argument());
+    
+    # Do not apply on unowned tickets
+    if ($self->TicketObj->OwnerObj->Id == RT->Nobody->Id) {
+        return;
+    }
+    
     return 1;
 }
 
@@ -19,6 +25,24 @@ sub Commit {
     my $self = shift;
     RT->Logger->debug(__PACKAGE__. ": Commit");
     RT->Logger->debug(__PACKAGE__. "::Argument: ". $self->Argument());
+    
+    if ($self->Argument =~ m/changeowner/) {
+        
+        RT->Logger->debug(__PACKAGE__. "::Commit Add owner as admincc");
+        
+        my $owner = $self->TicketObj->OwnerObj;
+        $self->TicketObj->AddWatcher(
+            Type => 'AdminCc',
+            PrincipalId => $owner->PrincipalId
+        );
+    }
+    
+    if ($self->Argument =~ m/nobody/) {
+        
+        RT->Logger->debug(__PACKAGE__. "::Commit Set owner to Nobody");
+        
+        $self->TicketObj->SetOwner(RT->Nobody, 'Force');
+    }
     return 1;
 }
 
